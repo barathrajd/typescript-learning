2031cce27c14f0577f90a47432ce3519fd3eb4e0
barathrajd <barathdeva757@gmail.com>
2022-11-23 22:54:08 +0530

    Objects

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var number1 = 1;
-var number2 = 10;
-function add(number1, number2, printValue, phrase) {
-  var result = number1 + number2;
-  if (printValue) {
-    console.log(phrase + result);
-  }
-  return number1 + number2;
-}
-add(number1, number2, true, "Result is ");
+var person = {
+    name: "John",
+    age: 30
+};
+console.log(person.nickname);
