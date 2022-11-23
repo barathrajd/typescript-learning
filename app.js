61b06556dcd49d6ed173e2aed74b4e6d15254476
barathrajd <barathdeva757@gmail.com>
2022-11-23 23:01:13 +0530

    Arrays

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: "John",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Coding"]
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobbie = _a[_i];
+    console.log(hobbie);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "John",
   age: 30,
+  hobbies: ["Sports", "Coding"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobbie of person.hobbies) {
+  console.log(hobbie);
+}
