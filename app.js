a6d2c01bb1672370b6a318a6c701779fae3bfcd4
barathrajd <barathdeva757@gmail.com>
2022-11-24 09:46:53 +0530

    Literal types

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // Union Type is pipe symbol |
 // e.g: number | string
-function combine(input1, input2) {
+function combine(input1, input2, 
+// Literal Type restriction to the these two types
+resultConversion) {
     var result;
     // Adding Runtime type checking to solve the confusion for the typescript
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(20, 30);
-var combinedName = combine("Barathraj", "D");
+var combinedAges = combine(20, 30, "as-number");
 console.log(combinedAges);
+var combinedStringAges = combine("20", "30", "as-number");
+console.log(combinedStringAges);
+var combinedName = combine("Barathraj", "D", "as-text");
 console.log(combinedName);
--- app.ts
+++ app.ts
@@ -3,20 +3,28 @@
 
 function combine(
   input1: number | string,
-  input2: number | string
+  input2: number | string,
+  // Literal Type restriction to the these two types
+  resultConversion: "as-number" | "as-text"
 ): number | string {
   let result;
   // Adding Runtime type checking to solve the confusion for the typescript
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(20, 30);
-const combinedName = combine("Barathraj", "D");
-
+const combinedAges = combine(20, 30, "as-number");
 console.log(combinedAges);
+
+const combinedStringAges = combine("20", "30", "as-number");
+console.log(combinedStringAges);
+
+const combinedName = combine("Barathraj", "D", "as-text");
 console.log(combinedName);
