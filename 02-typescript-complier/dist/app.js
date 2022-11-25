9a85df426bc1caa6a9ea89c800b51df9fefa0d18
barathrajd <barathdeva757@gmail.com>
2022-11-25 10:05:38 +0530

    Typescript compilation

---
+++ 02-typescript-complier/dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Barathraj");
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("Clicked");
+});
---
+++ 02-typescript-complier/src/app.ts
@@ -0,0 +1,6 @@
+console.log("Barathraj");
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("Clicked");
+});
