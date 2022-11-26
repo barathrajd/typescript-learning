4431060bc8b604c8b2908bc981634eff87b90c90
barathrajd <barathdeva757@gmail.com>
2022-11-26 09:23:05 +0530

    Interface implements

--- 03-classes-interface/dist/app.js
+++ 03-classes-interface/dist/app.js
@@ -1,11 +1,21 @@
 "use strict";
 let user1;
+class Person {
+    constructor(name) {
+        this.age = 23;
+        this.name = name;
+    }
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+}
 user1 = {
     name: "Barathraj",
-    age: 23,
     greet(phrase) {
         console.log(phrase + " " + this.name);
     },
 };
+user1 = new Person("Barath");
 user1.greet("Welcome");
+console.log(user1);
 //# sourceMappingURL=app.js.map
--- 03-classes-interface/src/app.ts
+++ 03-classes-interface/src/app.ts
@@ -1,17 +1,29 @@
-interface Person {
+interface Greetings {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+let user1: Greetings;
+
+class Person implements Greetings {
+  name: string;
+  age = 23;
+  constructor(name: string) {
+    this.name = name;
+  }
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
 
 user1 = {
   name: "Barathraj",
-  age: 23,
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
   },
 };
 
+user1 = new Person("Barath");
+
 user1.greet("Welcome");
+console.log(user1);
