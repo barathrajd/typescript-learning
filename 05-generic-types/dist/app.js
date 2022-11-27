ceb6d0a362b4db4bca0f7cbc59e2ad76a80caf7f
barathrajd <barathdeva757@gmail.com>
2022-11-27 23:17:25 +0530

    Generic Functions and Generic Function

--- 05-generic-types/dist/app.js
+++ 05-generic-types/dist/app.js
@@ -1,2 +1,8 @@
 "use strict";
+function merge(objectA, objectB) {
+    return Object.assign(objectA, objectB);
+}
+const mergedObject = merge({ name: "Barathraj" }, { age: 30 });
+const mergedObject2 = merge({ name: "Barathraj" }, { hobbies: ["Cooking"] });
+console.log(mergedObject);
 //# sourceMappingURL=app.js.map
--- 05-generic-types/src/app.ts
+++ 05-generic-types/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+// // Genetic type Array
+// const names: Array<string> = ["Barathraj", "Devarajan"];
+
+// // names[0].split(' ')
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve("This is done");
+//     resolve(10);
+//   }, 2000);
+//   reject(10);
+// });
+
+// promise.then((data) => {
+//   data.toString();
+// });
+
+function merge<T extends object, U extends object>(objectA: T, objectB: U) {
+  return Object.assign(objectA, objectB);
+}
+
+const mergedObject = merge({ name: "Barathraj" }, { age: 30 });
+const mergedObject2 = merge({ name: "Barathraj" }, { hobbies: ["Cooking"] });
+
+console.log(mergedObject);
