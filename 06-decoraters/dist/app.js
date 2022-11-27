3f7ea17a93e77f5c485f6a1dae3111e65497ba74
barathrajd <barathdeva757@gmail.com>
2022-11-28 00:27:29 +0530

    Multiple decoraters

--- 06-decoraters/dist/app.js
+++ 06-decoraters/dist/app.js
@@ -6,13 +6,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function Logger(logString) {
+    console.log("FACTORY");
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log("TEPMLATE FACTORY");
     return function (constructor) {
+        console.log("Rendering template...");
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -28,6 +31,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
+    Logger("LOGGING"),
     WithTemplate("<h2>My Person Object</h2>", "app")
 ], Person);
 const barath = new Person();
--- 06-decoraters/src/app.ts
+++ 06-decoraters/src/app.ts
@@ -1,5 +1,6 @@
 // factory Decoraters
 function Logger(logString: string) {
+  console.log("FACTORY");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -7,10 +8,11 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("TEPMLATE FACTORY");
   return function (constructor: any) {
+    console.log("Rendering template...");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
-
     if (hookEl) {
       hookEl.innerHTML = template;
       hookEl.querySelector("h2")!.textContent = p.name;
@@ -19,6 +21,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger("LOGGING - PERSON")
+@Logger("LOGGING")
 @WithTemplate("<h2>My Person Object</h2>", "app")
 class Person {
   name = "Barath";
