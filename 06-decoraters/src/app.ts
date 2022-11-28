fae2e1e2139cb8a8772e92cef8c715993f2b04a3
barathrajd <barathdeva757@gmail.com>
2022-11-28 22:31:03 +0530

    Decoraters

--- 06-decoraters/src/app.ts
+++ 06-decoraters/src/app.ts
@@ -34,3 +34,55 @@ class Person {
 const barath = new Person();
 
 console.log(barath);
+
+// --
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorater");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor Decorater");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log("Method Decorater");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Paramerter Decorater");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid Price, price must be positive");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
